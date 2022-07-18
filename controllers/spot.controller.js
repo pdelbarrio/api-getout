c3dd1e665b97babd480b33cf4897702c09be7bb0
Pablo del Barrio <pablodbb@gmail.com>
2022-07-18 20:29:37 +0200

    Updated spot controller to populate the uploader

--- controllers/spot.controller.js
+++ controllers/spot.controller.js
@@ -5,10 +5,11 @@ const getValidated = async (req, res, next) => {
   try {
     // const page = req.query.page ? parseInt(req.query.page) : 1;
     // const skip = (page - 1) * 20;
-    const spots = await Spot.find({ validated: true }).sort({
-      createdAt: "desc",
-    });
-    // .populate("uploader");
+    const spots = await Spot.find({ validated: true })
+      .sort({
+        createdAt: "desc",
+      })
+      .populate("uploader");
 
     return res.status(200).json({
       message: "Recovered validated spots",
